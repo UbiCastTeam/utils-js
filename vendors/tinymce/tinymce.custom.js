d57774e21570078db3212f15cf7d75cb7389ccff
Stéphane Schoorens <stephane.schoorens@ubicast.eu>
2017-05-15 10:22:29 +0200

    add jquery and tinymce in utils-js

---
+++ vendors/tinymce/tinymce.custom.js
@@ -0,0 +1,20 @@
+// Custom initialization script for Tiny MCE
+function tinymce_custom_init(options) {
+	$(document).ready(function () {
+		var opt = options ? options : {};
+		if (!opt.selector)
+			opt.mode = "textareas";
+		opt.menubar = false;
+		opt.convert_urls = false;
+		opt.plugins = ["code link"];
+		opt.toolbar_items_size = "small";
+		opt.fontsize_formats = "14px 15px 16px 18px 20px 24px 28px 32px 40px";
+		opt.toolbar1 = "undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | link unlink code | bullist numlist | outdent indent blockquote | fontsizeselect";
+		opt.setup = function () {
+			this.on('init', function () {
+				this.targetElm.required = false;
+			});
+		};
+		tinymce.init(opt);
+	});
+}
