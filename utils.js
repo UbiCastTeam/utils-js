f56c7a97d63290790cdb4d69faf381bc56ac66c0
Anthony Violo <anthony.violo@ubicast.eu>
2018-02-14 10:53:54 +0100

    Miss return ios version

--- utils.js
+++ utils.js
@@ -183,6 +183,7 @@ utils._get_user_agent();
 utils._get_ios_version = function () {
     var version = 0;
     version = parseFloat(("" + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,""])[1]).replace("undefined", "3_2").replace("_", ".").replace("_", "")) || false;
+    return version;
 };
 
 utils._get_os_name = function () {
