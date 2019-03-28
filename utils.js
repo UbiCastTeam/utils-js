73a21c76a729ec10bc9be190201c53dc41b76df6
sschoorens <stephane.schoorens@ubicast.eu>
2019-03-28 11:48:27 +0100

    add slugify refs #28493

--- utils.js
+++ utils.js
@@ -530,3 +530,11 @@ utils.attempt_focus = function (element) {
     utils.ignore_until_focus_changes = false;
     return (document.activeElement === element);
 };
+utils.slugify = function (text) {
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+};
