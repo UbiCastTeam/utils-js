4025adbf9342944549abf4dae237fab045a0d447
sschoorens <stephane.schoorens@ubicast.eu>
2019-06-19 10:01:00 +0200

    add paste plugin refs #29204

--- vendors/tinymce/tinymce.custom.js
+++ vendors/tinymce/tinymce.custom.js
@@ -7,10 +7,11 @@ function tinymce_custom_init(options) {
             opt.mode = "textareas";
         opt.menubar = false;
         opt.convert_urls = false;
-        opt.plugins = ["code link"];
+        opt.paste_data_images = true;
+        opt.plugins = ["code link paste"];
         opt.toolbar_items_size = "small";
         opt.fontsize_formats = "14px 15px 16px 18px 20px 24px 28px 32px 40px";
-        opt.toolbar1 = "undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | link unlink code | bullist numlist | outdent indent blockquote | fontsizeselect";
+        opt.toolbar1 = "paste undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | link unlink code | bullist numlist | outdent indent blockquote | fontsizeselect";
         opt.browser_spellcheck = true;
         opt.setup = function (editor) {
             this.on("init", function () {
