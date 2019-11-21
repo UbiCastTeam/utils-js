0b91d192806ec94a469a2b4a3d03a1e461b8cebe
Stéphane Diemer <stephane.diemer@ubicast.eu>
2019-11-21 15:19:33 +0100

    Updated tiny mce to version 5.1.1

--- vendors/tinymce/tinymce.custom.js
+++ vendors/tinymce/tinymce.custom.js
@@ -1,16 +1,20 @@
 /* globals tinymce */
 // Custom initialization script for Tiny MCE
+var tinymce_custom_global_options = {};
 function tinymce_custom_init(options) {
     $(document).ready(function () {
         var opt = options ? options : {};
+        for (var key in tinymce_custom_global_options) {
+            opt[key] = tinymce_custom_global_options[key];
+        }
         if (!opt.selector)
             opt.mode = 'textareas';
         opt.menubar = false;
+        opt.contextmenu = false;
         opt.convert_urls = false;
         opt.paste_data_images = true;
         opt.plugins = ['code link paste'];
-        opt.toolbar_items_size = 'small';
-        opt.fontsize_formats = '14px 15px 16px 18px 20px 24px 28px 32px 40px';
+        opt.fontsize_formats = '0.8em 0.9em 1em 1.2em 1.5em 2em 2.5em 3em';
         opt.toolbar = 'paste undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | link unlink | bullist numlist | outdent indent blockquote | fontsizeselect | code removeformat';
         opt.browser_spellcheck = true;
         opt.setup = function (editor) {
